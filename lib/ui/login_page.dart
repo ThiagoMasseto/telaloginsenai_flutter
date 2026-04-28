da45090a133d5fe2ed166c77bf33cfd7fac58f61
Thiago Masseto <thimasseto@gmail.com>
2026-04-28 16:08:47 -0300

    Add LoginPage widget for user authentication interface
    - Created a new LoginPage widget in login_page.dart
    - Implemented a scaffold with an AppBar and a login form
    - Included fields for email and password input
    - Added an "Entrar" button for submission

---
+++ lib/ui/login_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 200,
+        centerTitle: true,
+        title: Column(
+          children: [
+            Image.asset('assets/images/imagem_senai.png', width: 181, height: 39,),
+            SizedBox(height: 20,),
+            Text("Login", style: TextStyle(color: Colors.white), ),
+          ],
+          
+        ),
+        backgroundColor: Colors.black),
+        body: Column(
+          children: [
+            SizedBox(height: 50,),
+            Text("Faça Login para Acessar o Sistema!", style: TextStyle(fontSize:20 ),),
+            SizedBox(height: 60,),
+            Text("Email:"),
+            TextField(decoration: InputDecoration(hintText: "Insira seu email:"),),
+            SizedBox(height: 53,),
+            TextField(decoration: InputDecoration(hintText: "Insira sua senha:"),),
+            SizedBox(height: 27,),
+            ElevatedButton(onPressed: (){}, child: Text("Entrar"))
+          ],
+        ),
+    );
+  }
+}
